30f2eb65ece9b3bd27bfe5f27ecb375e2cf0decb
Jozef Rebjak <jozefrebjak@icloud.com>
2021-10-25 15:44:12 +0200

    Fix styling of CronJobLogModel with CI4 standards

--- src/Models/CronJobLogModel.php
+++ src/Models/CronJobLogModel.php
@@ -1,41 +1,34 @@
 <?php
+
 namespace Daycry\CronJob\Models;
 
+use CodeIgniter\Database\ConnectionInterface;
 use CodeIgniter\Model;
 use CodeIgniter\Validation\ValidationInterface;
-use CodeIgniter\Database\ConnectionInterface;
 
 class CronJobLogModel extends Model
 {
-    protected $DBGroup = 'default';
-
-    protected $table      = 'cronjob';
-
-    protected $primaryKey = 'id';
-
-    protected $useAutoIncrement = true;
-
-    protected $returnType     = 'object';
-
-    protected $useSoftDeletes = true;
-
-    protected $allowedFields = [ 'name', 'type', 'action', 'environment', 'output', 'error', 'start_at', 'end_at', 'duration', 'test_time' ];
-
-    protected $useTimestamps = true;
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
+    protected $DBGroup            = 'default';
+    protected $table              = 'cronjob';
+    protected $primaryKey         = 'id';
+    protected $useAutoIncrement   = true;
+    protected $returnType         = 'object';
+    protected $useSoftDeletes     = true;
+    protected $allowedFields      = ['name', 'type', 'action', 'environment', 'output', 'error', 'start_at', 'end_at', 'duration', 'test_time'];
+    protected $useTimestamps      = true;
+    protected $createdField       = 'created_at';
+    protected $updatedField       = 'updated_at';
+    protected $deletedField       = 'deleted_at';
     protected $validationRules    = [];
     protected $validationMessages = [];
     protected $skipValidation     = false;
 
     public function __construct(?ConnectionInterface &$db = null, ?ValidationInterface $validation = null)
     {
-        parent::__construct( $db, $validation );
+        parent::__construct($db, $validation);
     }
 
-    public function setTableName( $tableName )
+    public function setTableName($tableName)
     {
         $this->table = $tableName;
     }
